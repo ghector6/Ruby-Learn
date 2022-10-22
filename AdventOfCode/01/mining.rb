b044af2e1b335b310ad074139cc3a30eeff7d1c2
Hector Garcia Rosales <ghectormus@gmail.com>
2022-10-22 14:41:17 -0500

    Adding algorithm for second part

--- AdventOfCode/01/mining.rb
+++ AdventOfCode/01/mining.rb
@@ -14,11 +14,21 @@ class Mining
         end
         i.to_i
     end
+    def num_finder_II
+        hashcode = ""
+        i = -1
+        until hashcode[0..5]=="000000"
+            i += 1
+            new_key = @key + i.to_s
+            hashcode = Digest::MD5.hexdigest ( new_key)
+        end
+        i.to_i
+    end
         
 
 end
 
 if __FILE__ == $0
-    finder = Mining.new("iwrupvqb")
-    p finder.num_finder
+    finder = Mining.new("ckczppom")
+    p finder.num_finder_II
 end

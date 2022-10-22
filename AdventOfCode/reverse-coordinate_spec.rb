b8705022080abc7e48f7e3ee6271152e995f40f5
Hector Garcia Rosales <ghectormus@gmail.com>
2022-10-22 12:06:56 -0500

    Adding test for part 2

--- AdventOfCode/reverse-coordinate_spec.rb
+++ AdventOfCode/reverse-coordinate_spec.rb
@@ -15,6 +15,19 @@ RSpec.describe Coordinates2 do
             expect(test_nuestro3.route).to eq(2)
         end        
     end
+
+    context "Dada un cadena de instrucciones Santa y Robo_Santa alternaran para poder recorrer mas casas" do
+        it "Se espera la visitia de 3 casas con el siguiente comando ^v" do
+            test1_parte2 = Coordinates2.new("^v")
+            expect(test1_parte2.robo_route).to eq(3)
+        end
+        it "Es espera la visita de 3 casa con el siguiente comando ^>v<" do
+            test2_parte2 = Coordinates2.new("^>v<")
+            expect(test2_parte2.robo_route).to eq(3)
+        end
+
+    end
+    
 end
   
 

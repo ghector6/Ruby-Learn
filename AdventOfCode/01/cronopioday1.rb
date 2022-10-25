c0e47208ab42fc1e06c8983c3b1dfaaea36b554d
Hector Garcia Rosales <ghectormus@gmail.com>
2022-10-25 18:39:55 -0500

    Addig working algorithm with ericks help

--- AdventOfCode/01/cronopioday1.rb
+++ AdventOfCode/01/cronopioday1.rb
@@ -10,36 +10,44 @@ class Elevator2
             @count += 1
         elsif direction == ")"
             @count -= 1   
-        end             
+        end  
     end
+    
 
     def floor
         @floor.each do |p|
             counter(p)
         end
         puts @count        
+        @count = 0
     end
 
     def basement
         @floor.each do |p|
             @count_list << counter(p)            
         end
+        p @count_list.size
         i = 0
         flag = 0
         until flag == -1
             flag = @count_list[i]
             i += 1
         end
-        puts i                   
+        @count_list = []    
+        @count = 0        
+        puts i  
     end    
     
 end
 
 testinput = File.read("inputday1.txt")
 floor = Elevator2.new(testinput)
-floor.floor
+floor2 = Elevator2.new(testinput)
+
 cronopio = Elevator2.new(testinput)
-cronopio.basement
+
+floor2.basement
+floor2.basement
 
  
  #cronopio.basement

685d1103db21e14c69407b215e9d3e724ff95f52
Hector Garcia Rosales <ghectormus@gmail.com>
2022-10-31 22:24:01 -0600

    Adding algorithm with match?

--- AdventOfCode/05/nice_strings.rb
+++ AdventOfCode/05/nice_strings.rb
@@ -6,8 +6,8 @@ class Nice_Strings
     def nice?
         #c1 = !!(@strings.match (/([aeiuo][^aeiou]*[aeiou][^aeiou]*[aeiou])/))
         c1 = (@strings.scan /[aeiou]/).size >= 3
-        c2 = !!(@strings.match  /([a-z])\1/ )
-        c3 = !(@strings.match /(ab)|(cd)|(pq)|(xy)/ )
+        c2 = (@strings.match?  /([a-z])\1/ )
+        c3 = !(@strings.match? /(ab)|(cd)|(pq)|(xy)/ )
         return c1 && c2 && c3
         # if ((@strings.scan /[aeiou]/).size >=3) && (@strings.match /([a-z])\1/ )
         #     puts "pretty string"

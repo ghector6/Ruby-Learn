9c0afb09ab2102c9018dcc331f534baa0c16f6cc
Hector Garcia Rosales <ghectormus@gmail.com>
2022-11-02 22:51:31 -0600

    Adding working algorithms

--- AdventOfCode/02/cuboid.rb
+++ AdventOfCode/02/cuboid.rb
@@ -1,18 +1,26 @@
 class Cuboid 
     def initialize(line)
         @line = line.scan(/\d+/)
+        @parsed_nums = []
+        @area = []
+
     end
 
     def getInteger
-        parsed_nums = @line.collect do |p|
+        @parsed_nums = @line.collect do |p|
             p.to_i
         end
-        parsed_nums
-               
+        @parsed_nums 
     end
+
+      def getMaths
+        area = getInteger.insert(-1,getInteger[0]).each_cons(2).map {|m| m.reduce(:*)}
+        p (area.sum * 2 ) + area.min
+     end
 end
 
 
-
-present = Cuboid.new("2x3x4")
-puts present.digits
+if __FILE__ == $0
+  present = Cuboid.new("2x3x4")
+  present.getMaths
+end

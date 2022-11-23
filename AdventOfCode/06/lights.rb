4328d42a0f082986cf5f6901851fe900e63c6029
Hector Garcia Rosales <ghectormus@gmail.com>
2022-11-23 16:47:05 -0600

    Creating a class and a method instructions

--- AdventOfCode/06/lights.rb
+++ AdventOfCode/06/lights.rb
@@ -1,14 +1,9 @@
-require_relative "ligths"
-
-Rspec.describe Lights do
-	context "Given a set of instructions lights TURN ON " do
-		it "turns all the lights on when receiving: turn on 0,0 through 999,999" do 
-			grid = Lights.new
-			grid.instruction("turn on 0,0 through 999,999")
-			resultado = grid.lights_on 
-			expect(resultado).to eq(1000000)
+class Lights
+	def instruction(string)
+		if string.include?("turn on")
+			true
 		end
+
 	end
 end
-
 	
